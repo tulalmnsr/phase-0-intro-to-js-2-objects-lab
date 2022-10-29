74a9b548775c123c99a004bf04a056107936236c
Batoul El Mansour <elmansourbatoul@gmail.com>
2022-10-29 23:26:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee={
+    name:"Batoul",
+    streetAddress : "Saida",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = { ...employee};
+    newObj[key] = value;
+    return newObj;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee.name="Sam"
+    employee.streetAddress="12 Broadway"
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key){
+ const newObj = { ...employee};
+ delete newObj.name
+ return newObj
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name
+    return employee
+}
